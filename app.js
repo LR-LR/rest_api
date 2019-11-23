b8a835710170544678eb51a56747803ad0f80d62
LR-LR <elhaire.aboudou@gmail.com>
2019-11-23 15:41:40 +0100

    Good modifs

--- app.js
+++ app.js
@@ -21,53 +21,54 @@ mongoose
     }
   )
   .then(() => {
-    console.log('MONGODB Connected');
-    app
-      .use('/uploads', express.static('uploads'))
-      // Parsing the body
-      .use(express.urlencoded({ extended: true }))
-      .use(express.json())
-
-      // CORS handling
-      .use((req, res, next) => {
-        res
-          .header('Access-Control-Allow-Origin', '*')
-          .header(
-            'Access-Control-Allow-Headers',
-            'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-          );
-        if (req.method === 'OPTIONS') {
-          res.header(
-            'Access-Control-Allow-Methods',
-            'PUT, POST, PATCH, DELETE, GET'
-          );
-          return res.status(200).json({});
-        }
-        next();
-      })
-
-      // Routes handling
-      .use('/users', usersRoutes)
-      .use('/posts', postsRoutes)
-      .use('/comments', commentsRoutes)
-
-      // Errors handling
-      .use((req, res, next) => {
-        const err = new Error('Not found');
-        err.status = 404;
-        next(err);
-      })
-      .use((err, req, res, next) => {
-        res.status(err.status || 500);
-        res.json({
-          err: {
-            message: err.message
-          }
-        });
-      });
-
-    module.exports = app;
+    console.log('Connected to DB!');
   })
   .catch((err) => {
     console.log(err);
   });
+
+app
+  .use('/uploads', express.static('uploads'))
+  // Parsing the body
+  .use(express.urlencoded({ extended: true }))
+  .use(express.json())
+
+  // CORS handling
+  .use((req, res, next) => {
+    res
+      .header('Access-Control-Allow-Origin', '*')
+      .header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+      );
+    if (req.method === 'OPTIONS') {
+      res.header(
+        'Access-Control-Allow-Methods',
+        'PUT, POST, PATCH, DELETE, GET'
+      );
+      return res.status(200).json({});
+    }
+    next();
+  })
+
+  // Routes handling
+  .use('/users', usersRoutes)
+  .use('/posts', postsRoutes)
+  .use('/comments', commentsRoutes)
+
+  // Errors handling
+  .use((req, res, next) => {
+    const err = new Error('Not found');
+    err.status = 404;
+    next(err);
+  })
+  .use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({
+      err: {
+        message: err.message
+      }
+    });
+  });
+
+module.exports = app;
--- server.js
+++ server.js
@@ -6,4 +6,4 @@ const port = process.env.PORT;
 const server = http.createServer(app);
 
 server.listen(port);
-console.log('Server listening on port ' + port);
+console.log('Server listening on port ' + port + '!');
